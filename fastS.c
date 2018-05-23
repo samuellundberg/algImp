0565f08fce751bbad08c22d3acca7d99267e2fbe
LurreMcFly <38757085+LurreMcFly@users.noreply.github.com>
2018-05-23 17:19:51 +0200

    Update fastS.c

--- fastS.c
+++ fastS.c
@@ -4,288 +4,267 @@
 #include <stdio.h>
 #include <limits.h>
 
-typedef struct {
+typedef struct RationalNumber{
 	long long t;
 	long long n;
-} ratnum_t;
+} RationalNumber;
 
 
-int getLcd(long long a, long long b){
-	long long tmp = 1;
-	while (b!=0) {tmp = b; b = a % b; a = tmp;}
-	return tmp;
+
+void reduce(RationalNumber *rn){
+	long long a = rn->t;
+	long long b = rn->n;
+  long long c = a % b;
+  if (c < 0){
+    c = -c;
+    a = -a;
+  }
+  while(c != 0)
+  {
+      a = b;
+      b = c;
+      c = a % b;
+  }
+  long long mgn = b;    //kanske onödig
+  if(rn->n < 0)
+    mgn = - mgn;
+
+  rn->t = rn->t/mgn;
+	rn->n = rn->n/mgn;
 }
 
-int reduce(ratnum_t *r){
-	long long t = r->t; //collect value from struct
-	long long n = r->n;
-	long long lcd = getLcd(t ,n);
-	r->t = t/lcd; // change value in our struct
-	r->n = n/lcd;
-	return 0;
+void subq(RationalNumber *rm1, RationalNumber *rm2, RationalNumber *rm_res){
+	rm_res->t = (rm2->t*rm1->n)-(rm1->t*rm2->n);
+	rm_res->n = rm1->n*rm2->n;
+	reduce(rm_res);
 }
 
-int subq(ratnum_t *r1, ratnum_t *r2, ratnum_t *result){
-	result->t = -1* ((r1->t*r2->n)-(r2->t*r1->n));
-	result->n = r1->n*r2->n;
-	reduce(result);
-	return 0;
-}
-
-int ratcomp(ratnum_t *r1, ratnum_t *r2) {
-		double R1 = (double)r1->t/(double)r1->n;
-	double R2 = (double)r2->t/(double)r2->n;
-
-if (R1<R2) {
-
-		return 0; //the subtraction became negative --> r1<r2
-	}
-	return 1;
+int compare(RationalNumber *rn1, RationalNumber *rn2) {
+	float a = ((float)rn1->t)/rn1->n;
+	float b = ((float)rn2->t)/rn2->n;
+  if(a>b)
+    return 1;
+  else
+    return 0;
 
 }
 
-int divq(ratnum_t *r1, ratnum_t *r2){
-
-	r1->t = r1->t*r2->n;
-	if(r2->t==0){
-		r1->t = 0;
-	}else{
-		r1->n = r1->n*r2->t;
-	}
-	reduce(r1);
-
-	return 0;
+void divq(RationalNumber *rn1, RationalNumber *rn2){
+ long long a;
+ long long b;
+	a = rn1->t*rn2->n;
+	b = rn1->n*rn2->t;
+  rn1->t = a;
+  rn1->n = b;
+	reduce(rn1);
 }
 
-int swap_rows(size_t r, ratnum_t **ineq, int row1, int row2){
-	for(size_t j = 0; j<r+1 ; j++){// swap all of the elements in the row
-		ratnum_t tmp = ineq[row1][j]; // pekar på elementet jag vill lägga i tmp
-  	ineq[row1][j] = ineq[row2][j];
-		ineq[row2][j] = tmp;
-		}
-  return 0;
+void swapRows(RationalNumber **rm, int row1, int row2){
+    RationalNumber* temp = rm[row1];
+    rm[row1] = rm[row2];
+    rm[row2] = temp;
 }
 
-int sort_list( size_t r, size_t s,  ratnum_t **ineq, int *n1, int *n2){
+void sort(size_t cols, size_t rows,  RationalNumber **rm, int *npos, int *nnonzer){
 	int count = 0;
-	for(size_t i = 0; i<s; i++){
-    ratnum_t rat = ineq[i][r-1];
-
-                                 // get ratnum to compare w 0 do not want float
-		 if((rat.t > 0 && rat.n > 0 ) || (rat.t < 0 && rat.n < 0)){ 		//check if positive
-			if(count!=(int)i){
-				swap_rows(r, ineq, i, count ); // rad i flyttas upp
+	for(int i = 0; i < (int)rows; i++){
+    RationalNumber rn = rm[i][cols-1];
+		 if((rn.t > 0 && rn.n > 0 ) || (rn.t < 0 && rn.n < 0)){
+			if(count!=i){
+				swapRows(rm, i, count);
 		 	}
       count += 1;
 		 }
    }
-	*n1 = count; // change value of n1 - all positive values
+	*npos = count;
 
-	for(size_t i = count; i<s; i++){
-		ratnum_t rat = ineq[i][r-1]; // get ratnum to compare w 0 do not want float
-		if((rat.t < 0 && rat.n > 0 ) || (rat.t > 0 && rat.n < 0)){
-			if(count!=(int)i){ //do not swap w myself
-        swap_rows(r, ineq, count, i );
+	for(int i = count; i < (int)rows; i++){
+		RationalNumber rn = rm[i][cols-1];
+		if((rn.t < 0 && rn.n > 0 ) || (rn.t > 0 && rn.n < 0)){
+			if(count!=i){
+        swapRows(rm, count, i );
 			}
       count += 1;
 		}
 	}
-
-	*n2 = count; // change value of n2
-
-  return 0;
+	*nnonzer = count;
 }
 
-int divide_matrix(size_t r, ratnum_t **ineq, int *n2){
-	for (int i = 0; i < *n2; i++) {
-		ratnum_t *divrat = malloc(sizeof(ratnum_t));
-		*divrat = ineq[i][r-1];
-		for (int j = 0; j < (int)r+1; j++) {
-			divq(&ineq[i][j], divrat);
+void divideRow(size_t cols, RationalNumber **rm, int *nnonzer){
+  int i,j;
+  for (i = 0; i < *nnonzer; i++) {
+		RationalNumber *divrn = malloc(sizeof(RationalNumber));
+		*divrn = rm[i][cols-1];
+		for (j = 0; j < (int)cols+1; j++) {
+			divq(&rm[i][j], divrn);
 		}
-		free(divrat);
-
+		free(divrn);
 	}
-
-	return 0;
 }
 
 
 
-int combine_inequalities(size_t r, size_t s,  ratnum_t **ineq, ratnum_t **new_ineq, int *n1, int *n2){
+int combine_rmualities(size_t cols, size_t rows,  RationalNumber **rm, RationalNumber **new_rm, int *npos, int *nnonzer){
 	int count = 0;
-	for (int i = 0; i < *n1; i++) {
-		for (int k = *n1; k < *n2; k++) {
-			for (int j = 0; j < (int)r-1; j++) {
-				subq(&ineq[i][j], &ineq[k][j], &new_ineq[count][j]);
+	for (int i = 0; i < *npos; i++) {
+		for (int k = *npos; k < *nnonzer; k++) {
+			for (int j = 0; j < (int)cols-1; j++) {
+				subq(&rm[i][j], &rm[k][j], &new_rm[count][j]);
 			}
-			subq(&ineq[k][r], &ineq[i][r], &new_ineq[count][r-1]); // add c
+			subq(&rm[k][cols], &rm[i][cols], &new_rm[count][cols-1]); // add c
 			count = count + 1;
 
 		}
 	}
 
-	for (size_t i = *n2; i < s; i++) { // add the rest (=0)
-		for (size_t j = 0; j < r-1; j++) {
-			new_ineq[count][j] = ineq[i][j];
+	for (size_t i = *nnonzer; i < rows; i++) { // add the rest (=0)
+		for (size_t j = 0; j < cols-1; j++) {
+			new_rm[count][j] = rm[i][j];
 		}
-		new_ineq[count][r-1] = ineq[i][r];
+		new_rm[count][cols-1] = rm[i][cols];
 		count = count+1; // ska vara lika med s_new
 	}
 	return 0;
 }
 
-int set_br_Br(ratnum_t *c, int n1, int n2, int *Br_isset, int *br_isset, ratnum_t *br, ratnum_t *Br){
-	ratnum_t minrat;
-	minrat.t = INT_MAX;
-	minrat.n = 1;
-	ratnum_t comp;					//Ratnum used to comapre the substraction
+int set_br_Br(RationalNumber *c, int npos, int nnonzer, int *Br_isset, int *br_isset, RationalNumber *br, RationalNumber *Br){
+	RationalNumber rn_min;
+	rn_min.t = 2147483647;
+	rn_min.n = 1;
+	RationalNumber comp;					//Ratnum used to comapre the substraction
 	*Br_isset = 0;
 	double min;
 	double test;
-	for (int i = 0; i < n1; i++) {
-		subq(&c[i], &minrat, &comp);       //Subtract the max value from the selected value and saves in the first
-		min = (double)minrat.t/(double)minrat.n;
+	for (int i = 0; i < npos; i++) {
+		subq(&c[i], &rn_min, &comp);       //Subtract the max value from the selected value and saves in the first
+		min = (double)rn_min.t/(double)rn_min.n;
 		test = (double)c[i].t/(double)c[i].n;
 
 		if (min>test) {
-			minrat = c[i];
+			rn_min = c[i];
 
 			*Br_isset = 1;
 		}
 	}
-	ratnum_t maxrat;
-	maxrat.t = -INT_MAX;
-	maxrat.n = 1;
+	RationalNumber rn_max;
+	rn_max.t = -2147483647;
+	rn_max.n = 1;
 	*br_isset = 0;
-	for (int i = n1; i < n2; i++) {
-		double max = (double)maxrat.t/(double)maxrat.n;
+	for (int i = npos; i < nnonzer; i++) {
+		double max = (double)rn_max.t/(double)rn_max.n;
 		double test2 = (double)c[i].t/(double)c[i].n;
-		subq(&c[i], &maxrat, &comp);
+		subq(&c[i], &rn_max, &comp);
 	if(max<test2){
-			maxrat = c[i];
+			rn_max = c[i];
 			*br_isset = 1;
 		}
 	}
-	*Br = minrat;
-	*br = maxrat;
+	*Br = rn_min;
+	*br = rn_max;
 	return 0;
 }
 
-void break_function(){
+void bp(){
 
 }
-size_t u;
+// size_t u;
 bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows])
 {
- 	u = cols;
- 	size_t s = rows;
- 	size_t r = cols;
- 	int *n1 = malloc(sizeof(int));
- 	int *n2 = malloc(sizeof(int));
- 	ratnum_t **ineq = malloc(s*sizeof(ratnum_t *)); //kanske alloca eller skriva eget  istället? Se xmallox i bok.
-	for (size_t i = 0; i < s; i++) {
-		ineq[i] = malloc((r+1)*sizeof(ratnum_t));
+  int i,j;
+ 	int *npos = malloc(sizeof(int));
+ 	int *nnonzer = malloc(sizeof(int));
+ 	RationalNumber **rm = malloc(rows*sizeof(RationalNumber *)); //kanske alloca eller skriva eget  istället? Se xmallox i bok.
+	for (i = 0; i < (int)rows; i++) {
+		rm[i] = malloc((cols+1)*sizeof(RationalNumber));
 	}
 
  /* FIRST set matrix */
- for (size_t i = 0; i < s; i++) {
-	 for (size_t j = 0; j < r; j++) {
-		 ratnum_t rat; //borde gå att definiera bättre
+ for (i = 0; i < (int)rows; i++) {
+	 for (j = 0; j < (int)cols; j++) {
+		 RationalNumber rat; //borde gå att definiera bättre
 		 rat.t = a[i][j];
 		 rat.n = 1;
-		 ineq[i][j] = rat;		//I j-loopen
+		 rm[i][j] = rat;		//I j-loopen
 	 }
-	 ratnum_t ratc; //borde gå att definiera bättre
+	 RationalNumber ratc; //borde gå att definiera bättre
 	 ratc.t = c[i];
 	 ratc.n = 1;
-	 ineq[i][r] = ratc;
+	 rm[i][cols] = ratc;
  }
 
 
 	while(1){
 
-		if(r==1){
+		if(cols==1){
 			break;
 		}
 
-		/*sort_list (step2)*/
-		sort_list(r, s, ineq, n1, n2);
-		for (size_t i = 0; i < s; i++) {
-			for (size_t j = 0; j < r+1; j++) {
-						}
-		}
+		/*sort (step2)*/
+		sort(cols, rows, rm, npos, nnonzer);
+		divideRow(cols, rm, nnonzer);
 
-		divide_matrix(r, ineq, n2);
-
-
-		for (size_t i = 0; i < s; i++) {
-			for (size_t j = 0; j < r+1; j++) {
-						}
-		}
-
-		size_t s_new =(s - (*n2)) + (*n1)*((*n2)-(*n1));
+		int s_new =(rows - (*nnonzer)) + (*npos)*((*nnonzer)-(*npos));
 
 		if(s_new==0){
-			for (size_t i = 0; i < s; i++) {
-				free(ineq[i]);
+			for (i = 0; i < (int)rows; i++) {
+				free(rm[i]);
 			}
-			free(ineq);
-			free(n1);
-			free(n2);
+			free(rm);
+			free(npos);
+			free(nnonzer);
 			return 1;	//HAS SOLUTION
 		}
-		ratnum_t **new_ineq = malloc(s_new*sizeof(ratnum_t *)); //kanske alloca eller skriva eget  istället? Se xmallox i bok.
-		for (size_t i = 0; i < s_new; i++) {
-			new_ineq[i] = malloc((r)*sizeof(ratnum_t));
+		RationalNumber **new_rm = malloc(s_new*sizeof(RationalNumber *)); //kanske alloca eller skriva eget  istället? Se xmallox i bok.
+		for (i = 0; i < s_new; i++) {
+			new_rm[i] = malloc((cols)*sizeof(RationalNumber));
 		}
-		combine_inequalities(r, s, ineq, new_ineq, n1, n2);	//creating a new matrix with the new inequalities
+		combine_rmualities(cols, rows, rm, new_rm, npos, nnonzer);	//creating a new matrix with the new rmualities
 
-		for (size_t i = 0; i < s_new; i++) {
-			for (size_t j = 0; j < r; j++) {
+		for (i = 0; i < (int)s_new; i++) {
+			for (j = 0; j < (int)cols; j++) {
 						}
 		}
 
-		for (size_t i = 0; i < s; i++) {
-			free(ineq[i]);
+		for (i = 0; i < (int)rows; i++) {
+			free(rm[i]);
 		}
-		free(ineq);
+		free(rm);
 
-		ineq = malloc(s_new*sizeof(ratnum_t *));
-		for (size_t i = 0; i < s_new; i++) {
-			ineq[i] =  malloc((r)*sizeof(ratnum_t));
+		rm = malloc(s_new*sizeof(RationalNumber *));
+		for (i = 0; i < (int)s_new; i++) {
+			rm[i] =  malloc((cols)*sizeof(RationalNumber));
 		}
-		for (size_t i = 0; i < s_new; i++) {
-			for (size_t j = 0; j < r; j++) {
-				ineq[i][j] = new_ineq[i][j];
+		for (i = 0; i < (int)s_new; i++) {
+			for (j = 0; j < (int)cols; j++) {
+				rm[i][j] = new_rm[i][j];
 			}
 		}
 
-		for (size_t i = 0; i < s_new; i++) {
-			free(new_ineq[i]);
+		for (i = 0; i < (int)s_new; i++) {
+			free(new_rm[i]);
 		}
-		free(new_ineq);
+		free(new_rm);
 
-		s = s_new;
-		r = r-1;
+		rows = s_new;
+		cols = cols-1;
 
 
 	}
-	sort_list(r, s, ineq, n1, n2);
-	ratnum_t *br = malloc(sizeof(ratnum_t));
-	ratnum_t *Br = malloc(sizeof(ratnum_t));
+	sort(cols, rows, rm, npos, nnonzer);
+	RationalNumber *br = malloc(sizeof(RationalNumber));
+	RationalNumber *Br = malloc(sizeof(RationalNumber));
 	int *Br_isset = malloc(sizeof(int *));
 	int *br_isset = malloc(sizeof(int *));
 	int *solved = malloc(sizeof(int));
-	divide_matrix(r, ineq, n2);
+	divideRow(cols, rm, nnonzer);
 
-	ratnum_t (*ratc) = malloc(s*sizeof(ratnum_t));
-	for (size_t i = 0; i < s; i++) {
-		ratnum_t rat; //borde gå att definiera bättre
-		rat = ineq[i][r];
+	RationalNumber (*ratc) = malloc(rows*sizeof(RationalNumber));
+	for (i = 0; i < (int)rows; i++) {
+		RationalNumber rat; //borde gå att definiera bättre
+		rat = rm[i][cols];
 		ratc[i] = rat;
 	}
-	set_br_Br(ratc, *n1, *n2, Br_isset, br_isset, br, Br);
+	set_br_Br(ratc, *npos, *nnonzer, Br_isset, br_isset, br, Br);
 
 	if(*Br_isset == 0) {			//no upper limit
 		*solved = 1;
@@ -294,30 +273,30 @@ bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows]
 		*solved = 1;
 
 	}
-	if (*n2-s > 0) {		//the coefficient of xr is zero
-		for (size_t i = *n2; i < s; i++){
-			ratnum_t temp = ratc[i];
+	if (*nnonzer-rows > 0) {		//the coefficient of xr is zero
+		for (i = *nnonzer; i < (int)rows; i++){
+			RationalNumber temp = ratc[i];
 			if ((temp.t < 0 && temp.n > 0) || (temp.t > 0 && temp.n < 0 )) {
 				*solved = 0;
 
 			}
 		}
 	}
-	if(ratcomp(Br,br) == 0 && *Br_isset == 1 && *br_isset == 1) { //if both limits are set and the upper is lower then the lowest
+	if(compare(Br,br) == 0 && *Br_isset == 1 && *br_isset == 1) { //if both limits are set and the upper is lower then the lowest
 		*solved = 0;
 
-	}else if (ratcomp(Br,br) == 1 && *Br_isset == 1 && *br_isset == 1) {
+	}else if (compare(Br,br) == 1 && *Br_isset == 1 && *br_isset == 1) {
 		*solved = 1;
 	}
 	free(ratc);
-	for (size_t i = 0; i < s; i++) {
-		free(ineq[i]);
+	for (i = 0; i < (int)rows; i++) {
+		free(rm[i]);
 	}
-	free(ineq);
+	free(rm);
 	free(br);
 	free(Br);
-	free(n1);
-	free(n2);
+	free(npos);
+	free(nnonzer);
 	free(Br_isset);
 	free(br_isset);
 	int intsolved = *solved;
